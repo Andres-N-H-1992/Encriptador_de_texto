682586f7745ddf556836c7b072891f85b5c83023
Banner Andres <bannernunez@unicomfacauca.edu.co>
2024-07-03 02:53:48 -0500

    Añadi comentarios a todas las lineas  del codigo del JS

--- js/encriptador.js
+++ js/encriptador.js
@@ -1,121 +1,204 @@
-// ------- seleccion de todos los elementos ----------//
+// ------- Selección de todos los elementos ----------//
+
+// Selecciona el botón de encriptar
 const btnEncriptar = document.querySelector('.btn-encriptar');
+
+// Selecciona el campo de texto donde se ingresará el texto a encriptar
 const txtEncriptar = document.querySelector('.Encriptar');
+
+// Selecciona el elemento que muestra el aviso de texto
 const aviso = document.querySelector('.texto-aviso');
+
+// Selecciona el elemento donde se mostrará el texto encriptado o desencriptado
 const respuesta = document.querySelector('.evaluar');
+
+// Selecciona el contenedor principal de la tarjeta que contiene los resultados
 const contenido = document.querySelector('.tarjeta-contenedor');
-const btnCopiar = document.querySelector(".btn-copiar");
-const btnDesencriptar = document.querySelector(".btn-desencriptar")
 
-//-------Boton Encriptar------------//
+// Selecciona el botón para copiar el texto encriptado o desencriptado
+const btnCopiar = document.querySelector('.btn-copiar');
+
+// Selecciona el botón de desencriptar
+const btnDesencriptar = document.querySelector('.btn-desencriptar');
+
+
+
+
+
+
+
+//------- Botón Encriptar ------------//
+
+// Añade un evento al botón de encriptar que se ejecutará cuando se haga clic en él
 btnEncriptar.addEventListener('click', (e) => {
-	e.preventDefault();
-	let texto = txtEncriptar.value;
+    // Evita el comportamiento predeterminado del evento, en este caso, el envío de un formulario si el botón está dentro de uno
+    e.preventDefault();
 
-	let txt = texto.normalize('NFD').replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, '');
+    // Obtiene el valor del texto ingresado para encriptar
+    let texto = txtEncriptar.value;
 
-	if (texto == '') {
-		aviso.style.background = '#0A3871';
-		aviso.style.color = '#ffff';
-		aviso.style.fontweight = '800';
-		aviso.textContent = 'El campo de texto no debe estar vacio';
+    // Elimina acentos y caracteres especiales del texto
+    let txt = texto.normalize('NFD').replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, '');
 
-		setTimeout(() => {
-			aviso.removeAttribute('style');
-		}, 1500);
-	} else if (texto !== txt) {
-		aviso.style.background = '#0A3871';
-		aviso.style.color = '#FFFF';
-		aviso.style.fontWeight = '800';
-		aviso.textContent = 'No debe tener acentos y caracteres especiales';
+    // Verifica si el campo de texto está vacío
+    if (texto == '') {
+        aviso.style.background = '#0A3871';
+        aviso.style.color = '#ffff';
+        aviso.style.fontWeight = '800';
+        aviso.textContent = 'El campo de texto no debe estar vacío';
 
-		setTimeout(() => {
-			aviso.removeAttribute('style');
-		}, 1500);
-	} else if (texto !== texto.toLowerCase()) {
-		aviso.style.background = '#0A3871';
-		aviso.style.color = '#FFFF';
-		aviso.style.fontWeight = '800';
-		aviso.textContent = 'El texto debe ser todo en minúscula';
+        // Remueve el estilo del aviso después de 1500 milisegundos (1.5 segundos)
+        setTimeout(() => {
+            aviso.removeAttribute('style');
+        }, 1500);
+    }
+    // Verifica si el texto contiene acentos o caracteres especiales
+    else if (texto !== txt) {
+        aviso.style.background = '#0A3871';
+        aviso.style.color = '#FFFF';
+        aviso.style.fontWeight = '800';
+        aviso.textContent = 'No debe tener acentos y caracteres especiales';
 
-		setTimeout(() => {
-			aviso.removeAttribute('style');
-		}, 1500);
-	} else {
-		texto = texto.replace(/e/gm, 'enter');
-		texto = texto.replace(/i/gm, 'imes');
-		texto = texto.replace(/a/gm, 'ai');
-		texto = texto.replace(/o/gm, 'ober');
-		texto = texto.replace(/u/gm, 'ufat');
+        // Remueve el estilo del aviso después de 1500 milisegundos (1.5 segundos)
+        setTimeout(() => {
+            aviso.removeAttribute('style');
+        }, 1500);
+    }
+    // Verifica si el texto contiene letras mayúsculas
+    else if (texto !== texto.toLowerCase()) {
+        aviso.style.background = '#0A3871';
+        aviso.style.color = '#FFFF';
+        aviso.style.fontWeight = '800';
+        aviso.textContent = 'El texto debe ser todo en minúscula';
 
-		respuesta.innerHTML = texto;
-         btnCopiar.style.visibility ="inherit";
+        // Remueve el estilo del aviso después de 1500 milisegundos (1.5 segundos)
+        setTimeout(() => {
+            aviso.removeAttribute('style');
+        }, 1500);
+    }
+    // Si pasa todas las validaciones, procede a encriptar el texto
+    else {
+        // Sustituye las vocales por sus respectivas palabras
+        texto = texto.replace(/e/gm, 'enter');
+        texto = texto.replace(/i/gm, 'imes');
+        texto = texto.replace(/a/gm, 'ai');
+        texto = texto.replace(/o/gm, 'ober');
+        texto = texto.replace(/u/gm, 'ufat');
+
+        // Muestra el texto encriptado en el elemento respuesta
+        respuesta.innerHTML = texto;
+
+        // Hace visible el botón de copiar
+        btnCopiar.style.visibility = 'inherit';
+
+        // Elimina el contenedor principal que muestra las instrucciones
         contenido.remove();
-       
-	}
+    }
 });
 
 
 
+
+
+
+
 /*
------------boton desencriptar--------
+----------- Botón Desencriptar ----------
 */
 
+// Añade un evento al botón de desencriptar que se ejecutará cuando se haga clic en él
 btnDesencriptar.addEventListener('click', (e) => {
-	e.preventDefault();
-	let texto = txtEncriptar.value;
+    // Evita el comportamiento predeterminado del evento, en este caso, el envío de un formulario si el botón está dentro de uno
+    e.preventDefault();
 
-	let txt = texto.normalize('NFD').replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, '');
+    // Obtiene el valor del texto ingresado para desencriptar
+    let texto = txtEncriptar.value;
 
-	if (texto == '') {
-		aviso.style.background = '#0A3871';
-		aviso.style.color = '#ffff';
-		aviso.style.fontweight = '800';
-		aviso.textContent = 'El campo de texto no debe estar vacio';
+    // Elimina acentos y caracteres especiales del texto utilizando expresiones regulares
+    let txt = texto.normalize('NFD').replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, '');
 
-		setTimeout(() => {
-			aviso.removeAttribute('style');
-		}, 1500);
-	} else if (texto !== txt) {
-		aviso.style.background = '#0A3871';
-		aviso.style.color = '#FFFF';
-		aviso.style.fontWeight = '800';
-		aviso.textContent = 'No debe tener acentos y caracteres especiales';
+    // Verifica si el campo de texto está vacío
+    if (texto == '') {
+        // Configura el estilo del aviso para indicar que el campo no debe estar vacío
+        aviso.style.background = '#0A3871';
+        aviso.style.color = '#ffff';
+        aviso.style.fontWeight = '800';
+        aviso.textContent = 'El campo de texto no debe estar vacío';
 
-		setTimeout(() => {
-			aviso.removeAttribute('style');
-		}, 1500);
-	} else if (texto !== texto.toLowerCase()) {
-		aviso.style.background = '#0A3871';
-		aviso.style.color = '#FFFF';
-		aviso.style.fontWeight = '800';
-		aviso.textContent = 'El texto debe ser todo en minúscula';
+        // Remueve el estilo del aviso después de 1500 milisegundos (1.5 segundos)
+        setTimeout(() => {
+            aviso.removeAttribute('style');
+        }, 1500);
+    }
+    // Verifica si el texto contiene acentos o caracteres especiales
+    else if (texto !== txt) {
+        // Configura el estilo del aviso para indicar que no debe haber acentos o caracteres especiales
+        aviso.style.background = '#0A3871';
+        aviso.style.color = '#FFFF';
+        aviso.style.fontWeight = '800';
+        aviso.textContent = 'No debe tener acentos y caracteres especiales';
 
-		setTimeout(() => {
-			aviso.removeAttribute('style');
-		}, 1500);
-	} else {
-		texto = texto.replace(/enter/gm, 'e');
-		texto = texto.replace(/imes/gm, 'i');
-		texto = texto.replace(/ai/gm, 'a');
-		texto = texto.replace(/ober/gm, 'o');
-		texto = texto.replace(/ufat/gm, 'u');
+        // Remueve el estilo del aviso después de 1500 milisegundos (1.5 segundos)
+        setTimeout(() => {
+            aviso.removeAttribute('style');
+        }, 1500);
+    }
+    // Verifica si el texto contiene letras mayúsculas
+    else if (texto !== texto.toLowerCase()) {
+        // Configura el estilo del aviso para indicar que el texto debe ser todo en minúscula
+        aviso.style.background = '#0A3871';
+        aviso.style.color = '#FFFF';
+        aviso.style.fontWeight = '800';
+        aviso.textContent = 'El texto debe ser todo en minúscula';
 
-		respuesta.innerHTML = texto;
-         btnCopiar.style.visibility ="inherit";
+        // Remueve el estilo del aviso después de 1500 milisegundos (1.5 segundos)
+        setTimeout(() => {
+            aviso.removeAttribute('style');
+        }, 1500);
+    }
+    // Si pasa todas las validaciones, procede a desencriptar el texto
+    else {
+        // Sustituye las palabras por las vocales correspondientes para desencriptar el texto
+        texto = texto.replace(/enter/gm, 'e');
+        texto = texto.replace(/imes/gm, 'i');
+        texto = texto.replace(/ai/gm, 'a');
+        texto = texto.replace(/ober/gm, 'o');
+        texto = texto.replace(/ufat/gm, 'u');
+
+        // Muestra el texto desencriptado en el elemento respuesta
+        respuesta.innerHTML = texto;
+
+        // Hace visible el botón de copiar para copiar el texto desencriptado
+        btnCopiar.style.visibility = 'inherit';
+
+        // Elimina el contenedor principal que muestra las instrucciones una vez que se ha desencriptado el texto
         contenido.remove();
-       
-	}
+    }
 });
+
+
+
+
+
+
 
 
 /*
------------boton copiar--------
+----------- Botón Copiar ----------
 */
 
+// Añade un evento al botón de copiar que se ejecutará cuando se haga clic en él
 btnCopiar.addEventListener('click', (e) => {
-	e.preventDefault();
+    // Evita el comportamiento predeterminado del evento, en este caso, evitará el envío de un formulario si el botón está dentro de uno
+    e.preventDefault();
+
+    // Obtiene el elemento donde se encuentra el texto que se desea copiar
     let copiar = respuesta;
+
+    // Selecciona todo el contenido del elemento para copiarlo al portapapeles
     copiar.select();
-    document.execCommand("copy");
+
+    // Ejecuta el comando para copiar el texto seleccionado al portapapeles del sistema
+    document.execCommand('copy');
 });
+
